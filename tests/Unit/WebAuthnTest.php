28bd3e0b661d4dede23d9122ff4289d5e16516b6
toda <ytoda@d-forest-corp.com>
2024-07-22 10:59:45 +0900

    add test

--- tests/Unit/WebAuthnTest.php
+++ tests/Unit/WebAuthnTest.php
@@ -2,6 +2,8 @@
 
 namespace Faiare\LaravelWebAuthn\Tests\Unit;
 
+use Faiare\LaravelWebAuthn\Entities\PrepareChallengeForRegistration;
+use Faiare\LaravelWebAuthn\Entities\RegisterPublicKey;
 use Faiare\LaravelWebAuthn\Facades\WebAuthn;
 use Faiare\LaravelWebAuthn\Tests\TestCase;
 use Faiare\LaravelWebAuthn\WebAuthnHelper;
@@ -15,5 +17,24 @@ class WebAuthnTest extends TestCase
             WebAuthnHelper::class,
             WebAuthn::getFacadeRoot()
         );
+    }
+
+    public function test_prepare_challenge_for_registration()
+    {
+        $result = WebAuthn::prepareChallengeForRegistration(
+            username: 'test',
+            userid: 'test',
+            crossPlatform: true
+        );
+
+        $this->assertInstanceOf(PrepareChallengeForRegistration::class, $result);
+
+        $this->assertArrayHasKey('publicKey', $result->jsonSerialize());
+        $this->assertArrayHasKey('b64challenge', $result->jsonSerialize());
+    }
+
+    public function test_register()
+    {
+        // TODO: Implement test_register() method.
     }
 }
